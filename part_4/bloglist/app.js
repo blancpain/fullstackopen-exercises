3dcb49d59f2d63ca5f69823d0014c9cc5072799b
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-07-08 15:02:20 +0300

    Configure bloglist backend from Part 4 to to clear test DB in test mode for Part 5 ex

--- part_4/bloglist/app.js
+++ part_4/bloglist/app.js
@@ -29,6 +29,12 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(middleware.tokenExtractor);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
